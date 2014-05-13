eb8537d8ba85da01b9681944f554cdae9b520914
Sebastian <sb@flexpage.net>
2014-05-13 20:51:30 +0200

    [server] Use argparse to process commandline in cmd loop

--- server/console.py
+++ server/console.py
@@ -1,12 +1,54 @@
+import argparse
 import cmd
 
 class CacheFSConsole(cmd.Cmd):
     intro = 'CacheFS console. Type help or ? to list commands\n'
     prompt = 'cachefs: $'
 
+    def __init__(self, cache):
+        cmd.Cmd.__init__(self)
+        self.cache = cache
+
+    def cmdloop(self):
+        try:
+            cmd.Cmd.cmdloop(self)
+        except KeyboardInterrupt as e:
+            print('')
+            # Resume command loop
+            # self.cmdloop()
+            return True
+
+    def do_exit(self, arg):
+        """Quit the server"""
+        print('Ending server')
+        return True
+
     def do_status(self, arg):
-        """ """
-        print("Status")
+        """Print status information"""
+        print('Status')
+        pass
+
+    def do_fetch(self, arg):
+        """ -a --all
+            -r --recursive
+            [path] [directory]
+        """
+        print('Fetch: arg {}'.format(arg))
+        parser = argparse.ArgumentParser(prog='fetch')
+        parser.set_defaults(all=False, recursive=False)
+        parser.add_argument('path', help='path to file/directory to flush')
+        parser.add_argument('-a', '--all', dest='all', action='store_true',
+                            help='include everything')
+        parser.add_argument('-r', '--recursive', dest='recursive',
+                            action='store_true',
+                            help='include subdirectories')
+
+        try:
+            args = parser.parse_args(arg.split())
+        except SystemExit:
+            return False
+
+        # self.cache.fetch(args.path, args.all, args.recursive)
         pass
 
     def do_flush(self, arg):
@@ -14,7 +56,22 @@ class CacheFSConsole(cmd.Cmd):
             -r --recursive
             [path] [directory]
         """
-        print("Flush")
+        print('Flush: arg {}'.format(arg))
+        parser = argparse.ArgumentParser(prog='flush')
+        parser.set_defaults(all=False, recursive=False)
+        parser.add_argument('path', help='path to file/directory to flush')
+        parser.add_argument('-a', '--all', dest='all', action='store_true',
+                            help='include everything')
+        parser.add_argument('-r', '--recursive', dest='recursive',
+                            action='store_true',
+                            help='include subdirectories')
+
+        try:
+            args = parser.parse_args(arg)
+        except SystemExit:
+            return False
+
+        # self.cache.flush(args.path, args.all, args.recursive)
         pass
 
     def do_pin(self, arg):
@@ -22,6 +79,22 @@ class CacheFSConsole(cmd.Cmd):
             -r --recursive
             [path] [directory]
         """
-        print("Pin")
+        print('Pin: arg {}'.format(arg))
+        parser = argparse.ArgumentParser()
+        parser.set_defaults(all=False, recursive=False)
+        parser.add_argument('path', help='path to file/directory to flush')
+        parser.add_argument('-a', '--all', dest='all', action='store_true',
+                            help='include everything')
+        parser.add_argument('-r', '--recursive', dest='recursive',
+                            action='store_true',
+                            help='include subdirectories')
+
+        try:
+            args = parser.parse_args(arg)
+        except SystemExit:
+            return False
+
+        # Fetch
+        # self.cache.pin(args.path, args.all, args.recursive)
         pass
 
